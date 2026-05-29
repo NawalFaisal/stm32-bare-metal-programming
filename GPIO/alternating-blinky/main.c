ab28905daaaba42a72db7a9e82f3a67da0d63e07
NawalFaisal <nawal.fais@outlook.com>
2026-05-29 01:41:08 -0600

    Alternating blinkys

--- GPIO/alternating-blinky/main.c
+++ GPIO/alternating-blinky/main.c
@@ -5,30 +5,29 @@ typedef struct {
     volatile uint32_t RESERVED[4];
     volatile uint32_t ODR;
 } GPIO_TypeDef;
-   
 
 typedef struct {
     volatile uint32_t RESERVED[12];
     volatile uint32_t AHB1ENR;
+} RCC_TypeDef;
 
-}RCC_TypeDef;
+#define RCC ((RCC_TypeDef *)0x40023800)
+#define GPIOA ((GPIO_TypeDef *)0x40020000)
 
-#define RCC ((RCC_TypeDef *)0x40023800)  
-#define GPIOA ((GPIO_TypeDef *)0x40020000)  // treat the bytes at address 0x40020000 as if they were a GPIO_TypeDef struct
-
-int main(void){
-
-    RCC -> AHB1ENR |= (1<<0);
-    GPIOA -> MODER |= (1<<10);
-    GPIOA -> ODR |= (1<<5);
-    GPIOA -> MODER |= (1<<12);
-    GPIOA -> ODR &= -(1<<6);
-
-
-    while(1){
-        GPIOA -> ODR ^= (1<<5);
-        GPIOA -> ODR ^=(1<<5);
+int main(void) {
+    RCC->AHB1ENR |= (1 << 0);
+    GPIOA->MODER &= ~(3 << 10);
+    GPIOA->MODER |= (1 << 10);
+    GPIOA->ODR |= (1 << 5);      //start high
+    
+    // clear + configure
+    GPIOA->MODER &= ~(3 << 12);
+    GPIOA->MODER |= (1 << 12);
+    GPIOA->ODR &= ~(1 << 6);       //start low
+    
+    while(1) {
+        GPIOA->ODR ^= (1 << 5);
+        GPIOA->ODR ^= (1 << 6);
         for(volatile int i = 0; i < 100000; i++);
     }
-
 }
