de27da22ea14048f837d8d69d4237c42c5bc51c3
NawalFaisal <nawal.fais@outlook.com>
2026-05-29 00:10:52 -0600

    Blinky on PA5

---
+++ GPIO/button-input-blinky/main.c
@@ -0,0 +1,25 @@
+
+// //TODO Goal — press the button, LED on PA5 turns on. Release it, LED turns off.
+
+// typedef struct GPIO{
+
+// volatile uint32_t MODER; 
+// volatile uint32_t RERSERVED[4];
+// volatile uint32_t ODR;
+// } GPIO_TypeDef;
+
+// typedef struct RCC{
+//     volatile uint32_t RESERVED[12];
+//     volatile uint32_t AHB1ENR;
+// } RCC_TypeDef;
+
+// #define GPIOC ((GPIO_TypeDef *) 0x40020800)
+// #define RCC  ((RCC_TypeDef *) 0x40023800)
+
+
+// int main(void){
+//     while(1){
+        
+
+//     }
+// }
