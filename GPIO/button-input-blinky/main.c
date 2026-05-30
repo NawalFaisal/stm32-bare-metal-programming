3c2ebfb415be78bfb6ec46fcad1e7946e338f165
NawalFaisal <nawal.fais@outlook.com>
2026-05-30 17:09:37 -0600

    turned led on/off with user button

--- GPIO/button-input-blinky/main.c
+++ GPIO/button-input-blinky/main.c
@@ -1,25 +1,72 @@
+#include <stdint.h>
 
-// //TODO Goal — press the button, LED on PA5 turns on. Release it, LED turns off.
+//goal use user button click on it and pa6 should turn on and off for each click
+//the user button is connected to the I/O PC13
 
-// typedef struct GPIO{
+typedef struct GPIO{
 
-// volatile uint32_t MODER; 
-// volatile uint32_t RERSERVED[4];
-// volatile uint32_t ODR;
-// } GPIO_TypeDef;
-
-// typedef struct RCC{
-//     volatile uint32_t RESERVED[12];
-//     volatile uint32_t AHB1ENR;
-// } RCC_TypeDef;
-
-// #define GPIOC ((GPIO_TypeDef *) 0x40020800)
-// #define RCC  ((RCC_TypeDef *) 0x40023800)
+volatile uint32_t MODER; 
+volatile uint32_t RESERVED[3];
+volatile uint32_t IDR;
+volatile uint32_t ODR;
+} GPIO_TypeDef;
 
 
-// int main(void){
-//     while(1){
+
+typedef struct RCC{
+    volatile uint32_t RESERVED[12];
+    volatile uint32_t AHB1ENR;
+} RCC_TypeDef;
+
+#define GPIOC ((GPIO_TypeDef *) 0x40020800)  
+#define GPIOA ((GPIO_TypeDef *) 0x40020000)
+#define RCC  ((RCC_TypeDef *) 0x40023800)
+
+
+int main(void){
+    RCC -> AHB1ENR |= (1<<0);  //GPIOA
+    RCC -> AHB1ENR |= (1<<2);
+
+    //clear bits 10 11 and set to output
+    GPIOA -> MODER &= ~(3<<10);
+    GPIOA -> MODER |= (1<<10);
+
+     //clear bit 5 and set it to off intially
+    GPIOA -> ODR &= ~(1<<5);
+
+    //clear mode for safety
+    //PC13 is at bits 2627 so shift left 3 to get 00
+    GPIOC -> MODER &= ~(3<<26);
+
+    //variable 
+    int led_state = 0;
+    int last_button_state = 0;
+
+    while(1){
         
+        int button_pressed = !(GPIOC -> IDR >>13) & 1;
+          //toggle states
+        if(button_pressed == 1 && last_button_state == 0){
+            if(led_state == 0){
+                led_state = 1;
+            } else{
+                led_state = 0;
+            }
+        }
 
-//     }
-// }
+        //update led based on its state
+        if(led_state == 1 ){
+            GPIOA -> ODR |= (1<<5);
+        }   else{
+            GPIOA -> ODR &= ~(1<<5);
+        }
+        last_button_state = button_pressed;
+        //debounce delay
+        for (volatile int i = 0; i < 10000; i++);
+
+    }
+
+
+
+}
+
